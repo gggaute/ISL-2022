d57aeedf27f6536713945c37d57ef9061ffb3e02
Gaute Brandser <gautbra@stud.ntnu.no>
2022-03-22 18:29:26 +0100

    (#39) removed navbar neste oppgave

--- isl/src/components/NavBar/Navbar.js
+++ isl/src/components/NavBar/Navbar.js
@@ -9,11 +9,9 @@ const Navbar = () => {
         <img id="logo" src={logo} onClick={() => {window.open("https://www.ntnu.edu/learnnowyouth/info/welcome", "_blank")}} alt="Norsk for Ungdom logo" width="130"></img>
       </div>
       <div className='mid-div'>
-        <h1 id="navbarTitle">Nytt Navn</h1>
+        <h1 id="navbarTitle">HVA SKAL STÅ HER</h1>
       </div>
       <div className='right-div'>
-        <button id="previousQ"><FcPrevious/> {"Forrige"}</button>
-        <button id="nextQ">{"Neste"} <FcNext/></button>
       </div>
     </div>
   )
