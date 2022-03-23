d3941117fa9d9357e277c8da1b621b8e20b6d1b4
synnej <synnej@stud.ntnu.no>
2022-03-23 21:31:44 +0100

    (#24) Fixed card width

--- isl/src/components/SetCard/styles.js
+++ isl/src/components/SetCard/styles.js
@@ -2,15 +2,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   cardHeader: {
-    padding: '12px'
+    padding: '12px',
   },
   card: {
     marginTop: '3px',
     marginRight: theme.spacing(1),
     marginLeft: theme.spacing(1),
-    [theme.breakpoints.between('sm', 'xl')]: {
-      width: '60vw',
-    },
+    // [theme.breakpoints.between('sm', 'xl')]: {
+    //   width: '60vw',
+    // },
   },
   link: {
     textDecoration: 'none',
