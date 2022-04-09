46de7f4a2d9e077b340ff16b87946d9dcebf9ad0
Skattum <aksesk@stud.ntnu.no>
2022-04-09 13:20:01 +0200

    deleting extra images

--- isl/src/components/NavBar/Navbar.js
+++ isl/src/components/NavBar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/images/logo.png";
 import { FcPrevious, FcNext } from "react-icons/fc";
 import HomeIcon from "@mui/icons-material/Home";
 import { IconButton } from "@mui/material";
