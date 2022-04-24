08206037bb4b9f8ab960ed4d7a1a176564f1199f
synnej <synnej@stud.ntnu.no>
2022-04-24 17:11:18 +0200

    (#58) Comments in overviewPage-comp

--- isl/src/components/OverviewPage/OverviewPage.js
+++ isl/src/components/OverviewPage/OverviewPage.js
@@ -1,9 +1,19 @@
 // import React, { useState, useEffect } from "react";
-import { Card, Typography, Button, Paper } from "@mui/material";
+import { Typography, Button, Paper } from "@mui/material";
 import NavBar from "../NavBar/Navbar";
 import "./styles.css";
 
-
+/**
+ * A component with some information about the set clicked on StartPage, and a button to start playing
+ * @author Ingvild, Synne
+ * @param {object} property
+ * @property {integer} setId    The ID of the set
+ * @property {string} setTitle  The title of the set
+ * @property {integer} totalExercises   Number of exercises in the set
+ * @property {object} listOfExerciseTypes   List of different exercisetypes in the set
+ * @property {function} nextExercise    Function to route to the first exercise in the set
+ * @returns The overviewPage-component
+ */
 export default function OverviewPage({ setId, setTitle, totalExercises, listOfExerciseTypes, nextExercise }) {
     return (
         <>
