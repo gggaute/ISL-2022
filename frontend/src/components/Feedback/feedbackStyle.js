32d78a69fb65485d518cd913c101f208d7dd2664
Ingvild <ingvisov@stud.ntnu.no>
2022-05-01 20:29:29 +0200

    points box fix

--- frontend/src/components/Feedback/feedbackStyle.js
+++ frontend/src/components/Feedback/feedbackStyle.js
@@ -1,12 +1,15 @@
 // Styling for the Feedback-component
 
 import { makeStyles } from '@mui/styles';
+import { margin } from '@mui/system';
 
 const useStyles = makeStyles((theme) => ({
   // Points: Point-count (i.e. '0 / 1')
   points: {
     backgroundColor: 'lightgreen',
     padding: "15px",
+    margin: "auto",
+    width: '50%',
   },
   // BtnParent: Container for next exercise button
   btnParent: {
